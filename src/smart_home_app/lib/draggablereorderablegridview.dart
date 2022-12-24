9b014c64940e7b9df569f29bcf6260be2f34836d
Sioxox <sioxox@pm.me>
2022-12-24 15:34:34 +0100

    First try of changing app icon

--- src/smart_home_app/lib/draggablereorderablegridview.dart
+++ src/smart_home_app/lib/draggablereorderablegridview.dart
@@ -1,8 +1,10 @@
-// New step, make the widget staggered with different sizes
-// Final name: Draggable Reorderable Staggered GridView
+// Next step:
+// - make the widget staggered with different sizes
+// Final name: Draggable Reorderable Staggered Quilted GridView
 
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter_shake_animated/flutter_shake_animated.dart';
 
 class DraggableReorderableGridView extends StatefulWidget {
   final List<Widget> items;
@@ -24,44 +26,48 @@ class _DraggableReorderableGridViewState extends State<DraggableReorderableGridV
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(items.length, (index) {
-          final item = items[index];
-          return DragTarget<Widget>(
-            onAccept: (Widget data) {
-              setState(() {
-                final int oldIndex = items.indexOf(data);
-                items.removeAt(oldIndex);
-                items.insert(index, data);
-              });
-            },
-            builder: (context, accepted, rejected) {
-              return LongPressDraggable<Widget>(
-                data: item,
-                child: item,
-                feedback: Container(
+    return GridView.count(
+      crossAxisCount: 2,
+      // crossAxisSpacing: 0,
+      // mainAxisSpacing: 0,
+      children: List.generate(items.length, (index) {
+        final item = items[index];
+        return DragTarget<Widget>(
+          onAccept: (Widget data) {
+            setState(() {
+              final int oldIndex = items.indexOf(data);
+              items.removeAt(oldIndex);
+              items.insert(index, data);
+            });
+          },
+          builder: (context, accepted, rejected) {
+            return LongPressDraggable<Widget>(
+              data: item,
+              child: item,
+              feedback: ShakeWidget(
+                child: Container(
                   width: 200,
                   height: 200,
                   child: item,
                 ),
-                childWhenDragging: Container(
-                  margin: EdgeInsets.all(15),
-                  child: DottedBorder(
-                    color: Colors.grey,
-                    dashPattern: [12, 8],
-                    strokeWidth: 2,
-                    borderType: BorderType.RRect,
-                    radius: Radius.circular(5),
-                    child: Container(),
-                  ),
+                shakeConstant: ShakeLittleConstant1(),
+                autoPlay: true,
+              ),
+              childWhenDragging: Container(
+                margin: EdgeInsets.all(15),
+                child: DottedBorder(
+                  color: Colors.grey,
+                  dashPattern: [12, 8],
+                  strokeWidth: 2,
+                  borderType: BorderType.RRect,
+                  radius: Radius.circular(5),
+                  child: Container(),
                 ),
-              );
-            },
-          );
-        }),
-      ),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 }
