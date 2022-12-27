3535bc2c7f2aa2e79bb77a6cbcc55a0384ac5460
Sioxox <sioxox@pm.me>
2022-12-27 14:26:34 +0100

    Addition of the app_theme file model

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData light = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.light,
+  );
+
+  static final ThemeData dark = ThemeData(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.dark,
+  );
+}
