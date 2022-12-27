640e2f11e2ec7a5def6bd15d9933c5f4c93ff95f
Sioxox <sioxox@pm.me>
2022-12-27 13:07:17 +0100

    Creation of main file structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'draggablereorderablegridview.dart';
+import 'features/home/widgets/draggablereorderablegridview.dart';
 void main() {
   runApp(
     MaterialApp(
