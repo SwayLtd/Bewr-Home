af0dcb4c078e3c9f48a884ec09db242d715f5966
Sioxox <sioxox@pm.me>
2022-12-27 14:26:21 +0100

    Addition of the routes file model

---
+++ lib/core/routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class Routes {
+  static const String home = '/';
+  static const String settings = '/settings';
+
+  static Map<String, WidgetBuilder> get routes {
+    return {
+      // home: (context) => HomePage(),
+      // settings: (context) => SettingsPage(),
+    };
+  }
+}
