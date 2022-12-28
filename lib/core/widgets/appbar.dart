513fca4e8c37ecb9d4fad3869beced1df64028a5
Sioxox <sioxox@pm.me>
2022-12-28 01:51:38 +0100

    Need to make the app bar and the drawer persistent to all pages

---
+++ lib/core/widgets/appbar.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Widget appBar() {
+   return PreferredSize(
+    preferredSize: const Size.fromHeight(100),
+      child: AppBar(
+      // https://stackoverflow.com/questions/52489458/how-to-change-status-bar-color-in-flutter
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.white, // Status bar color
+
+        // Status bar brightness (optional)
+        statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+        statusBarBrightness: Brightness.light, // For iOS (dark icons)
+      ),
+      iconTheme: const IconThemeData(color: Colors.black),
+      backgroundColor: Colors.white,
+      title: const Text('Pryve', style: TextStyle(color: Colors.black)),
+      elevation: 0.5, // configure the separator line under the AppBar
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person_outline),
+            onPressed: () {
+              // Accédez aux paramètres utilisateur ici
+            },
+          ),
+        ],
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(48),
+        child: SizedBox(
+          height: 48,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              TextButton(
+                child: const Text(
+                  'Home',
+                  style: TextStyle(
+                    // color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacementNamed(context, Routes.home);
+                },
+              ),
+              TextButton(
+                child: const Text(
+                  'Devices',
+                  style: TextStyle(
+                    // color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacementNamed(context, Routes.home);
+                },
+              ),
+              TextButton(
+                child: const Text(
+                  'Automations',
+                  style: TextStyle(
+                    // color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacementNamed(context, Routes.home);
+                },
+              ),
+              TextButton(
+                child: const Text(
+                  'Activity',
+                  style: TextStyle(
+                    // color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacementNamed(context, Routes.home);
+                },
+              ),
+              TextButton(
+                child: const Text(
+                  'Settings',
+                  style: TextStyle(
+                    // color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacementNamed(context, Routes.home);
+                },
+              ),
+              TextButton(
+                child: const Text(
+                  'Test',
+                  style: TextStyle(
+                    // color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacementNamed(context, Routes.home);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+/*
+Theme(
+  data: Theme.of(context).copyWith(
+    buttonTheme: const ButtonThemeData(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.transparent, width: 0),
+      ),
+    ),
+  ),
+  child: Stack(
+    children: [
+      Positioned(
+        left: 0,
+        right: 0,
+        bottom: 0,
+        child: Container(
+          height: 2,
+          // color: _pageIndex == 0 ? Colors.blue : Colors.transparent,
+          color: Colors.transparent,
+        ),
+      ),
+      TextButton(
+        child: const Text(
+          'Home',
+          style: TextStyle(
+            // color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+            color: Colors.grey,
+          ),
+        ),
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, Routes.home);
+        },
+      ),
+    ],
+  ),
+),
+*/
---
+++ lib/core/widgets/drawer.dart
@@ -0,0 +1,87 @@
+// https://betterprogramming.pub/how-to-implement-a-navigation-drawer-in-flutter-8d97d3b599d4
+
+import 'package:flutter/material.dart';
+import 'package:smart_home_app/core/routes.dart';
+
+Widget createDrawer(BuildContext context) {
+  return Drawer(
+    child: Column(
+      children: <Widget>[
+        Expanded(
+          // ListView contains a group of widgets that scroll inside the drawer
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.home_outlined),
+                title: const Text('Home'),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, Routes.home);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.devices_other_outlined),
+                title: const Text('Devices'),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, Routes.devices);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.auto_awesome_outlined),
+                title: const Text('Automations'),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, Routes.automations);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.notifications_outlined),
+                title: const Text('Activity'),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, Routes.activity);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        ),
+        // This container holds the align
+        Align(
+            alignment: FractionalOffset.bottomCenter,
+            // This container holds all the children that will be aligned
+            // on the bottom and should not scroll with the above ListView
+            child: Column(
+              children: <Widget>[
+            const Divider(color: Colors.grey),
+            ListTile(
+              leading: const Icon(Icons.settings_outlined),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, Routes.settings);
+                Navigator.of(context).pop();
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.feedback_outlined),
+              title: const Text('Feedback'),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, Routes.feedback);
+                Navigator.of(context).pop();
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.help_outline),
+              title: const Text('Help'),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, Routes.help);
+                Navigator.of(context).pop();
+              },
+            ),
+              ],
+            ),
+        )
+      ],
+    ),
+  );
+}
