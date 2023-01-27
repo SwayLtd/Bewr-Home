e7a846b16e87963409e8514f5441b9efe1b5fb22
Sioxox <sioxox@pm.me>
2023-01-27 22:11:57 +0100

    Theme integration for the app bar

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -1,4 +1,7 @@
+import 'package:bewr_home/core/routes.dart';
+import 'package:bewr_home/core/widgets/navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
@@ -9,20 +12,46 @@ class AppTheme {
     primarySwatch: Colors.amber,
     brightness: Brightness.light,
     fontFamily: GoogleFonts.spaceGrotesk().fontFamily,
+    disabledColor: Colors.black54,
+    appBarTheme: const AppBarTheme(
+      // https://stackoverflow.com/questions/52489458/how-to-change-status-bar-color-in-flutter
+      systemOverlayStyle: SystemUiOverlayStyle(
+        // statusBarColor: Colors.white, // Status bar color
+
+        // Status bar brightness (optional)
+        statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+        statusBarBrightness: Brightness.light, // For iOS (dark icons)
+      ),
+      elevation: 0.5, // configure the separator line under the AppBar
+      color: Colors.white,
+    ),
     textTheme: TextTheme(
-      displayLarge: GoogleFonts.spaceGrotesk(fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-      displayMedium: GoogleFonts.spaceGrotesk(fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-      displaySmall: GoogleFonts.spaceGrotesk(fontSize: 48, fontWeight: FontWeight.w400),
-      headlineMedium: GoogleFonts.spaceGrotesk(fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-      headlineSmall: GoogleFonts.spaceGrotesk(fontSize: 24, fontWeight: FontWeight.w400),
-      titleLarge: GoogleFonts.spaceGrotesk(fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-      titleMedium: GoogleFonts.spaceGrotesk(fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-      titleSmall: GoogleFonts.spaceGrotesk(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-      bodyLarge: GoogleFonts.spaceGrotesk(fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-      bodyMedium: GoogleFonts.spaceGrotesk(fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-      labelLarge: GoogleFonts.spaceGrotesk(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-      bodySmall: GoogleFonts.spaceGrotesk(fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-      labelSmall: GoogleFonts.spaceGrotesk(fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+      displayLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+      displayMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+      displaySmall:
+          GoogleFonts.spaceGrotesk(fontSize: 48, fontWeight: FontWeight.w400),
+      headlineMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+      headlineSmall:
+          GoogleFonts.spaceGrotesk(fontSize: 24, fontWeight: FontWeight.w400),
+      titleLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+      titleMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+      titleSmall: GoogleFonts.spaceGrotesk(
+          fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+      bodyLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+      bodyMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+      labelLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+      bodySmall: GoogleFonts.spaceGrotesk(
+          fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+      labelSmall: GoogleFonts.spaceGrotesk(
+          fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
     ).apply(
       bodyColor: Colors.black,
       displayColor: Colors.black,
@@ -34,20 +63,46 @@ class AppTheme {
     primarySwatch: Colors.amber,
     brightness: Brightness.dark,
     fontFamily: GoogleFonts.spaceGrotesk().fontFamily,
+    disabledColor: Colors.white70,
+    appBarTheme: const AppBarTheme(
+      // https://stackoverflow.com/questions/52489458/how-to-change-status-bar-color-in-flutter
+      systemOverlayStyle: SystemUiOverlayStyle(
+        // statusBarColor: Colors.white, // Status bar color
+
+        // Status bar brightness (optional)
+        statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+        statusBarBrightness: Brightness.light, // For iOS (dark icons)
+      ),
+      elevation: 0.5, // configure the separator line under the AppBar
+      color: Colors.white10,
+    ),
     textTheme: TextTheme(
-      displayLarge: GoogleFonts.spaceGrotesk(fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-      displayMedium: GoogleFonts.spaceGrotesk(fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-      displaySmall: GoogleFonts.spaceGrotesk(fontSize: 48, fontWeight: FontWeight.w400),
-      headlineMedium: GoogleFonts.spaceGrotesk(fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-      headlineSmall: GoogleFonts.spaceGrotesk(fontSize: 24, fontWeight: FontWeight.w400),
-      titleLarge: GoogleFonts.spaceGrotesk(fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-      titleMedium: GoogleFonts.spaceGrotesk(fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-      titleSmall: GoogleFonts.spaceGrotesk(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-      bodyLarge: GoogleFonts.spaceGrotesk(fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-      bodyMedium: GoogleFonts.spaceGrotesk(fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-      labelLarge: GoogleFonts.spaceGrotesk(fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-      bodySmall: GoogleFonts.spaceGrotesk(fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-      labelSmall: GoogleFonts.spaceGrotesk(fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+      displayLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+      displayMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+      displaySmall:
+          GoogleFonts.spaceGrotesk(fontSize: 48, fontWeight: FontWeight.w400),
+      headlineMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+      headlineSmall:
+          GoogleFonts.spaceGrotesk(fontSize: 24, fontWeight: FontWeight.w400),
+      titleLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+      titleMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+      titleSmall: GoogleFonts.spaceGrotesk(
+          fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+      bodyLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+      bodyMedium: GoogleFonts.spaceGrotesk(
+          fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+      labelLarge: GoogleFonts.spaceGrotesk(
+          fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+      bodySmall: GoogleFonts.spaceGrotesk(
+          fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+      labelSmall: GoogleFonts.spaceGrotesk(
+          fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
     ).apply(
       bodyColor: Colors.white,
       displayColor: Colors.white,
