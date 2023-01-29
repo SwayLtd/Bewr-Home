d0f4ed405ab2d9bf6633c3e9467da9dae62f90d7
Sioxox <sioxox@pm.me>
2023-01-29 14:25:03 +0100

    Optimization of the code and its readability

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -28,13 +28,15 @@ final GoRouter router = GoRouter(
         return ResponsiveWrapper.builder(
           Stack(
             children: <Widget>[
-              ResponsiveVisibility( // Show bottom navigation bar only on mobiles (smaller than TABLET)
+              ResponsiveVisibility(
+                // Show bottom navigation bar only on mobiles (smaller than TABLET)
                 hiddenWhen: const [
                   Condition.largerThan(name: MOBILE),
                 ],
                 child: ScaffoldWithNavBar(child: child),
               ),
-              ResponsiveVisibility( // Show app bar only on tablets and desktops (larger than MOBILE)
+              ResponsiveVisibility(
+                // Show app bar only on tablets and desktops (larger than MOBILE)
                 hiddenWhen: const [
                   Condition.smallerThan(name: TABLET),
                 ],
@@ -44,9 +46,12 @@ final GoRouter router = GoRouter(
           ),
           defaultScale: true,
           breakpoints: const [
-            ResponsiveBreakpoint.resize(480, name: MOBILE), // 480 is the default size
-            ResponsiveBreakpoint.autoScale(800, name: TABLET), // 800 is the default size
-            ResponsiveBreakpoint.resize(1000, name: DESKTOP), // 1000 is the default size
+            ResponsiveBreakpoint.resize(480,
+                name: MOBILE,), // 480 is the default size
+            ResponsiveBreakpoint.autoScale(800,
+                name: TABLET,), // 800 is the default size
+            ResponsiveBreakpoint.resize(1000,
+                name: DESKTOP,), // 1000 is the default size
             ResponsiveBreakpoint.autoScale(2460, name: '4K'),
           ],
         );
@@ -116,54 +121,34 @@ final GoRouter router = GoRouter(
   errorBuilder: (context, state) => const HomePage(), // TODO: Add error screen
 );
 
-int selectedIndex() { // Returns the index of the current page
-  final String location = router.location;
-  if (location.startsWith('/home')) {
-    return 0;
-  }
-  if (location.startsWith('/devices')) {
-    return 1;
-  }
-  if (location.startsWith('/automations')) {
-    return 2;
-  }
-  if (location.startsWith('/activity')) {
-    return 3;
-  }
-  if (location.startsWith('/settings')) {
-    return 4;
-  }
-  if (location.startsWith('/feedback')) {
-    return 5;
-  }
-  if (location.startsWith('/help')) {
-    return 6;
-  }
-  if (location.startsWith('/test')) {
-    return 7;
-  }
-  return 0;
+final routes = [
+  {'path': '/home', 'index': 0, 'name': 'Home'},
+  {'path': '/devices', 'index': 1, 'name': 'Devices'},
+  {'path': '/automations', 'index': 2, 'name': 'Automations'},
+  {'path': '/activity', 'index': 3, 'name': 'Activity'},
+  {'path': '/settings', 'index': 4, 'name': 'Settings'},
+  {'path': '/feedback', 'index': 5, 'name': 'Feedback'},
+  {'path': '/help', 'index': 6, 'name': 'Help'},
+  {'path': '/test', 'index': 7, 'name': 'Test'},
+];
+
+// Return the current route name
+String routeName() {
+  final route = routes.firstWhere((route) => route['path'] == router.location, orElse: () => routes.first);
+  return route['name'] as String ?? "Error"; // TODO: Add error screen
 }
 
-void onTap(BuildContext context, int index) { // Navigates to the page corresponding to the index
-  switch (index) {
-    case 0:
-      return context.push('/home');
-    case 1:
-      return context.push('/devices');
-    case 2:
-      return context.push('/automations');
-    case 3:
-      return context.push('/activity');
-    case 4:
-      return context.push('/settings');
-    case 5:
-      return context.push('/feedback');
-    case 6:
-      return context.push('/help');
-    case 7:
-      return context.push('/test');
-    default: // Need to check if this is not creating a loop when we are faking the index for the bottom navigation bar
-      return context.push('/home'); // If the index is not valid, navigate to the home page
-  }
+// Returns the index of the current screen
+int selectedIndex() {
+  final route = routes.firstWhere((route) => route['path'] == router.location, orElse: () => routes.first);
+  return route['index'] as int ?? 0; // TODO: Add error screen
+}
+
+// Navigates to the screen corresponding to the index
+void onTap(BuildContext context, int index) {
+  final route = routes.firstWhere((route) => route['index'] == index, orElse: () => routes.first);
+
+  // context.push as been used instead of context.go because it works better with the back button
+  // Need to check if this is not creating a loop when we are faking the index for the bottom navigation bar
+  return context.push(route['path'] as String ?? "/home"); // TODO: Add error screen
 }
